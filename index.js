f1ec2e29acdcf2883a50861969c6ec2166ead3be
Wani4ka <contact.wani4ka@gmail.com>
2019-01-21 01:07:13 +0300

    Playing with tick period

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ io.on('connection', (socket) => {
             players.get(id).y = y0 + vy * t;
             io.emit('move', {uid: id, x: players.get(id).x, y: players.get(id).y});
             ++t;
-        }, 1);
+        }, 10);
     });
     socket.on('disconnect', () => {
         players.remove(id);
