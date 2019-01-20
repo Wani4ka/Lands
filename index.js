ded9e0ca5a05ce62beae0604d14c3d3e89e48da7
Wani4ka <contact.wani4ka@gmail.com>
2019-01-20 23:33:23 +0300

    Testing Socket.IO framework

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 5000;
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-express()
+app
   .get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
-    .get('/storage/:type/:file', (req, res) => res.sendFile(__dirname + '/storage/' + req.params.type + '/' + req.params.file))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .get('/storage/:type/:file', (req, res) => res.sendFile(__dirname + '/storage/' + req.params.type + '/' + req.params.file));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+http.listen(PORT, () => console.log(`Listening on ${ PORT }`));
