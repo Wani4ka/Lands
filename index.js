fdd4b61e4316a227f67d8023dd0afec3dea03a53
Wani4ka <contact.wani4ka@gmail.com>
2019-01-21 21:53:09 +0300

    VK integration.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Vk = require('vk-node-sdk');
 const app = new Vk.App(process.env.APP_TOKEN);
 
 exp
-  .get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+  .get('/', (req, res) => {res.sendFile(__dirname + '/index.html'))
   .get('/storage/:type/:file', (req, res) => res.sendFile(__dirname + '/storage/' + req.params.type + '/' + req.params.file));
 const players = new Map();
 
@@ -31,47 +31,56 @@ const walkSpeed = parseFloat(process.env.WALK_SPEED);
 
 io.on('connection', (socket) => {
     let id = socket.id;
-    io.emit('draw', {uid: id, info: {skin: 'character', x: 385, y: 385}});
-    for (let player of players)
-        socket.emit('draw', {uid: player[0], info: player[1]});
-    players.set(id, {skin: 'character', x: 385, y: 385});
-    let moveId = -1;
-    socket.on('move', (pos) => {
-        let mx = pos.x;
-        let my = pos.y;
-        if (mx > 765 || my > 491 || mx < 0 || my < 0) return;
-        let x0 = players.get(id).x;
-        let y0 = players.get(id).y;
-        let t = 0;
-        let time = dst(x0, y0, mx, my) / walkSpeed;
-        let vx = (mx - x0) / time;
-        let vy = (my - y0) / time;
-        if (moveId !== -1) clearInterval(moveId);
-        moveId = setInterval(() => {
-            if (t >= time) {
-                clearInterval(moveId);
-                moveId = -1;
-                return;
-            }
-            for (let i = 0; i < fence.length; ++i)
-                if (dst(x0 + vx * t, y0 + vy * t, fence[i][0], fence[i][1]) < 3) {
+    socket.on('auth', (res) => {
+        let obj;
+        try {
+            obj = JSON.parse(res);
+            id = obj.response.firstName + ' ' + obj.response.lastName;
+        } catch (err) {
+            io.emit('error', err.message);
+        }
+        io.emit('draw', {uid: id, info: {skin: 'character', x: 385, y: 385}});
+        for (let player of players)
+            socket.emit('draw', {uid: player[0], info: player[1]});
+        players.set(id, {skin: 'character', x: 385, y: 385});
+        let moveId = -1;
+        socket.on('move', (pos) => {
+            let mx = pos.x;
+            let my = pos.y;
+            if (mx > 765 || my > 491 || mx < 0 || my < 0) return;
+            let x0 = players.get(id).x;
+            let y0 = players.get(id).y;
+            let t = 0;
+            let time = dst(x0, y0, mx, my) / walkSpeed;
+            let vx = (mx - x0) / time;
+            let vy = (my - y0) / time;
+            if (moveId !== -1) clearInterval(moveId);
+            moveId = setInterval(() => {
+                if (t >= time) {
                     clearInterval(moveId);
                     moveId = -1;
                     return;
                 }
-            players.get(id).x = x0 + vx * t;
-            players.get(id).y = y0 + vy * t;
-            io.emit('move', {uid: id, x: players.get(id).x, y: players.get(id).y});
-            ++t;
-        }, parseInt(process.env.TICK_PERIOD));
-    });
-    socket.on('chat', (msg) => {
-        io.emit('chat', id + ': ' + msg);
-    });
+                for (let i = 0; i < fence.length; ++i)
+                    if (dst(x0 + vx * t, y0 + vy * t, fence[i][0], fence[i][1]) < 3) {
+                        clearInterval(moveId);
+                        moveId = -1;
+                        return;
+                    }
+                players.get(id).x = x0 + vx * t;
+                players.get(id).y = y0 + vy * t;
+                io.emit('move', {uid: id, x: players.get(id).x, y: players.get(id).y});
+                ++t;
+            }, parseInt(process.env.TICK_PERIOD));
+        });
+        socket.on('chat', (msg) => {
+            io.emit('chat', id + ': ' + msg);
+        });
 
-    socket.on('disconnect', () => {
-        players.delete(id);
-        io.emit('remove', id);
+        socket.on('disconnect', () => {
+            players.delete(id);
+            io.emit('remove', id);
+        });
     });
 });
 http.listen(PORT, () => console.log(`Listening on ${ PORT }`));
